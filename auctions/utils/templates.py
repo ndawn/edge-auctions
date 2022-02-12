8ce91fbcd5ffa8154378ba8c919890d48e84858e
Max <burmistrovm@live.ru>
2022-02-12 17:23:45 +0300

    fixes

--- auctions/utils/templates.py
+++ auctions/utils/templates.py
@@ -1,39 +1,67 @@
 import re
-from typing import Union
 
-from auctions.comics.models import Item
-from auctions.supply.models import SupplyItem
+from auctions.auctioneer.models import Auction
+from auctions.config import DEFAULT_TIMEZONE
 
 
 TEMPLATE_TAG_MAP = [
-    ('name', lambda item: item.name),
-    ('type', lambda item: item.type.name),
-    ('buy_now_price', lambda item: (item.price_category or item.type.price_category).buy_now_price),
-    ('buy_now_expires', lambda item: (item.price_category or item.type.price_category).buy_now_expires),
-    ('bid_start_price', lambda item: (item.price_category or item.type.price_category).bid_start_price),
-    ('bid_min_step', lambda item: (item.price_category or item.type.price_category).bid_min_step),
-    ('bid_multiple_of', lambda item: (item.price_category or item.type.price_category).bid_multiple_of),
+    (
+        'name',
+        lambda auction: auction.item.name,
+    ),
+    (
+        'type',
+        lambda auction: auction.item.type.name,
+    ),
+    (
+        'buy_now_price',
+        lambda auction: auction.buy_now_price,
+    ),
+    (
+        'buy_now_expires',
+        lambda auction: auction.buy_now_expires,
+    ),
+    (
+        'bid_start_price',
+        lambda auction: auction.bid_start_price,
+    ),
+    (
+        'bid_min_step',
+        lambda auction: auction.bid_min_step,
+    ),
+    (
+        'bid_multiple_of',
+        lambda auction: auction.bid_multiple_of,
+    ),
+    (
+        'date_due',
+        lambda auction: auction.date_due.astimezone(DEFAULT_TIMEZONE).strftime('%d.%m в %H:%M'),
+    ),
+    (
+        'anti_sniper',
+        lambda auction: auction.set.anti_sniper,
+    ),
 ]
 
 
 WRAP_TAG_NAME = 'include'
 
 
-def build_template(text: str, item: Union[Item, SupplyItem]) -> str:
+def build_template(text: str, auction: Auction) -> str:
     output = ''
 
     for tag, get_tag in TEMPLATE_TAG_MAP:
         if re.search(r'{ ?%s ?}' % tag, text) is not None:
-            output = re.sub(r'{ ?%s ?}' % tag, get_tag(item), text)
+            output = re.sub(r'{ ?%s ?}' % tag, str(get_tag(auction)), text)
 
     return output
 
 
-async def build_description(item: Union[Item, SupplyItem]) -> str:
-    description = item.description
-    await item.fetch_related('wrap_to')
-    if item.wrap_to is not None:
-        description = re.sub(r'{ ?%s ?}' % WRAP_TAG_NAME, description, item.wrap_to.text)
+async def build_description(auction: Auction) -> str:
+    description = auction.item.description
+    await auction.fetch_related('item__wrap_to', 'item__price_category', 'item__type__price_category', 'set')
+    if auction.item.wrap_to is not None:
+        description = re.sub(r'{ ?%s ?}' % WRAP_TAG_NAME, description, auction.item.wrap_to.text)
 
-    description = build_template(description, item)
+    description = build_template(description, auction)
     return description
