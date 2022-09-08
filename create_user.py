ee95ffa468a14aee204ccc2e15851f5d77817ce2
Max <burmistrovm@live.ru>
2022-09-08 04:00:14 +0400

    fixes

--- create_user.py
+++ create_user.py
@@ -1,14 +1,23 @@
+import os
 import sys
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from auctions.db.repositories.users import UsersRepository
 from auctions.services.users_service import UsersService
 
 
-service = UsersService(None, UsersRepository())  # type: ignore
+app = Flask(__name__)
 
-service.create_user(
-    username=sys.argv[1],
-    password=sys.argv[2],
-    first_name=sys.argv[3],
-    last_name=sys.argv[4],
-)
+with app.app_context():
+    db = SQLAlchemy(os.getenv("DB_URL"))
+
+    service = UsersService(None, UsersRepository())  # type: ignore
+
+    service.create_user(
+        username=sys.argv[1],
+        password=sys.argv[2],
+        first_name=sys.argv[3],
+        last_name=sys.argv[4],
+    )
