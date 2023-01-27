aab7fce714943e3a617a9d98a440247301e716a3
Max <burmistrovm@live.ru>
2023-01-27 21:24:17 +0100

    fixes

--- uvicorn_config.py
+++ uvicorn_config.py
@@ -1,9 +1,17 @@
+import sys
 from multiprocessing import cpu_count
 
 import uvicorn
 
 
 def run_configured(app):
+    bind_kwargs = {}
+
+    if sys.platform == "win32":
+        bind_kwargs["port"] = 1337
+    else:
+        bind_kwargs["uds"] = "./sockets/app.sock"
+
     config = uvicorn.Config(
         app,
         host="0.0.0.0",
